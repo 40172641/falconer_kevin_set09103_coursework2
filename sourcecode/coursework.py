3f151039b1e9578d4d1637d19ad5a8c491112732
40172641 <40172641@live.napier.ac.uk>
2017-10-21 06:12:45 +0000

    Error handling

--- sourcecode/coursework.py
+++ sourcecode/coursework.py
@@ -1,9 +1,14 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def route():
-  return "CW2"
+  return render_template('home.html')
+
+#Error Handler
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('error.html'),404
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
